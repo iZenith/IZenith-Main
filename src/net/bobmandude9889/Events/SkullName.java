26bc19eb19d69801969e40223b236cffc4ef34fa
BuildTools <unconfigured@null.spigotmc.org>
2015-08-10 18:41:15 -0500

    Added get name of skulls

---
+++ src/net/bobmandude9889/Events/SkullName.java
@@ -0,0 +1,28 @@
+package net.bobmandude9889.Events;
+
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.Skull;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.block.Action;
+
+public class SkullName implements Listener{
+
+	@EventHandler
+	public void onClick(final PlayerInteractEvent e){
+		Action a = e.getAction();
+		if (a.equals(Action.RIGHT_CLICK_BLOCK)){
+			Player p = e.getPlayer();
+			BlockState b = e.getClickedBlock().getState();
+			if (b instanceof Skull){
+				Skull s = (Skull) b;
+				String owner = s.getOwner();
+				p.sendMessage(ChatColor.YELLOW + "That is the head of " + ChatColor.GOLD + owner + ChatColor.YELLOW + ".");			
+			}
+		}
+		e.setCancelled(true);
+	}
+}
