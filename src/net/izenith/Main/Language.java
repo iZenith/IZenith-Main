2c5dc2e35f779998357677f7d2a49d6227db8103
Will Garrett <willgarrett610@gmail.com>
2015-09-10 21:16:32 -0600

    Finished Language enum

--- src/net/izenith/Main/Language.java
+++ src/net/izenith/Main/Language.java
@@ -16,6 +16,7 @@ public enum Language {
 	DANISH("da"),
 	DUTCH("nl"),
 	ESTONIAN("et"),
+	ENGLISH("en"),
 	FINNISH("fi"),
 	FRENCH("fr"),
 	GALICIAN("gl"),
@@ -25,12 +26,60 @@ public enum Language {
 	HEBREW("he"),
 	HUNGARIAN("hu"),
 	ICELANDIC("is"),
-	INDONESIAN("id");
+	INDONESIAN("id"),
+	ITALIAN("it"),
+	JAPANESE("ja"),
+	KAZAKH("kk"),
+	KOREAN("ko"),
+	LATVIAN("lv"),
+	LITHUANIAN("lt"),
+	MACEDONIAN("mk"),
+	MALAY("ms"),
+	MONGOLIAN("mn"),
+	NORWEGIAN("nb"),
+	POLISH("pl"),
+	PORTUGUESE("pt"),
+	ROMANIAN("ro"),
+	RUSSIAN("ru"),
+	SLOVAK("sk"),
+	SLOVENIAN("sl"),
+	SPANISH("es"),
+	SWAHILI("sw"),
+	SWEDISH("sv"),
+	TAGALOG("tl"),
+	TATAR("tt"),
+	THAI("th"),
+	TURKISH("tr"),
+	UKRAINIAN("uk"),
+	UZBEK("uz"),
+	VIETNAMESE("vi"),
+	WELSH("cy");
 	
 	private String code;
 	
 	private Language(String code){
 		this.code = code;
+	}
+	
+	public String code(){
+		return code;
+	}
+	
+	public static Language getByCode(String code){
+		for(Language l : values()){
+			if(l.code().equalsIgnoreCase(code))
+				return l;
+		}
+		return null;
+	}
+	
+	public static Language getByName(String name){
+		for(Language l : values()){
+			if(l.name().equalsIgnoreCase(name)){
+				return l;
+			}
+		}
+		return null;
 	}
 	
 }
